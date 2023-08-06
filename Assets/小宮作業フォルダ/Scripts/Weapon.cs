08f500a497220fd016076415c2350cdc54cd1618
ColonelBaby <iuyamiko20060729@gmail.com>
2023-08-06 21:05:46 +0900

    敵を倒した時のエフェクトの追加

--- Assets/小宮作業フォルダ/Scripts/Weapon.cs
+++ Assets/小宮作業フォルダ/Scripts/Weapon.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    public GameObject Effect;
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Enemy")
         {
+            var effect = Instantiate(Effect);
+            effect.transform.position = other.transform.position;
             Destroy(other.gameObject);
+            Destroy(effect, 5);
         }
     }
 }
