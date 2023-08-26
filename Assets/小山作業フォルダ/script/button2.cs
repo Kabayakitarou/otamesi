fcfbd914a95c429297c2b109fb94fd482f40e06d
DESKTOP-GL2I1NJ\Ruki <rukikk01@gmail.com>
2023-08-26 17:55:10 +0900

    no message

---
+++ Assets/小山作業フォルダ/script/button2.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class button2 : MonoBehaviour
+{
+    
+
+    public void Onclick()
+    {
+        SceneManager.LoadScene("time");
+    }
+}
+
